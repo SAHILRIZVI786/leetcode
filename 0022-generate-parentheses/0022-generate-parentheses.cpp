2fb88a71abcc8b980c91778a54605cc305557690
SAHILRIZVI786 <rizvisahil084@gmail.com>
2026-07-08 00:57:08 +0530

    Time: 3 ms (70.32%), Space: 12.9 MB (89.97%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -4,24 +4,24 @@ public:
     void solve(int open, int close, int n,
                string &curr, vector<string> &ans) {
 
-        // Valid string formed
+    
         if (curr.length() == 2 * n) {
             ans.push_back(curr);
             return;
         }
 
-        // Add '(' if available
+    
         if (open < n) {
             curr.push_back('(');
             solve(open + 1, close, n, curr, ans);
-            curr.pop_back();     // Backtrack
+            curr.pop_back();   
         }
 
-        // Add ')' only if it won't make string invalid
+    
         if (close < open) {
             curr.push_back(')');
             solve(open, close + 1, n, curr, ans);
-            curr.pop_back();     // Backtrack
+            curr.pop_back();     
         }
     }
 
